f134359263ff7eb1c99f59a6a999e518e1ff55f8
essbee808 <sheena.myb@gmail.com>
2018-05-06 23:58:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ function whisper(string) {
   return string.toLowerCase()
 }
 
-function logShout(hello) {
-  console.log(logShout.toUpperCase(hello))
+function logShout(string) {
+  console.log(string.toUpperCase())
 }
-
